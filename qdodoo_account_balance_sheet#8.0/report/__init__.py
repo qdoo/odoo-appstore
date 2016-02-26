44a74310eb07c88eeba34a4493ea57702abfb4d8
tfs <979581151@qq.com>
2016-02-26 15:29:37 +0800

    新增中国会计-科目余额表

---
+++ qdodoo_account_balance_sheet#8.0/report/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+###########################################################################################
+#    author:Adger Zhou
+#    module name for OpenERP
+#    Copyright (C) 2015 qdodoo Technology CO.,LTD. (<http://www.qdodoo.com/>).
+#
+###########################################################################################
+
+import qdodoo_account_balance_report
